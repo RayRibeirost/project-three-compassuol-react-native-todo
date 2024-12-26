f6caa3c02a96bfc7ef7bd3f72857af00014b4d8a
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2024-12-26 17:34:28 -0300

    constants created fonts implemented 26_12_24

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Regular' : require('./assets/fonts/Inter_28pt-Regular.ttf'),
+    'Inter-SemiBold' : require('./assets/fonts/Inter_28pt-SemiBold.ttf'),
+    'Inter-Bold' : require('./assets/fonts/Inter_28pt-Bold.ttf')
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Init</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/constants/colors.ts
@@ -0,0 +1,21 @@
+export const colors = {
+    principal : {
+        purpleLight : "#DDD2EF",
+        purpleBase: "#9359F3",
+        purpleDark : "#6F3CC3",
+        greenLight : "#BFE3D0",
+        greenBase : "#479C6E",
+        greenDark : "#2D6C4A"
+    },
+    base : {
+        gray100 : "#F0EDF2",
+        gray200 : "#E5E2E9",
+        gray300 : "#E0DCE4",
+        gray400 : "#D1CBD7",
+        gray500 : "#6B6572",
+        gray600 : "#262428"
+    },
+    feedback : {
+        danger : "#C2462D"
+    },
+}
